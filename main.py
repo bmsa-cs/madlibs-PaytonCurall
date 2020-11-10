d079bab81f5403587a0bf983def3949add835c86
Payton Curall <pcurall24@biomedscienceacademy.org>
2020-11-10 13:58:30 +0000

    this is my first draft.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ Period/Core:
 
 
 """
+print("Let's play Silly Sentences!")
+enter_name = input("please enter a name: ")
+enter_adjective = input("please enter a adjective: ")
+enter_noun = input("please enter a noun: ")
+enter_color = input("please enter a color: ")
+enter_noun2 = input("please enter another noun: ")
+enter_place = input("please enter a place: ")
+enter_adjective2 = input("please enter a adjective: ")
+enter_gender = input("please enter a gender: ")
+print(enter_name + (" has a ") + enter_adjective + (" ") + enter_noun + (". ") + enter_name + ("'s favorite color is ") + enter_color + (". So everything in her house is ") + enter_color + (", even her ") + enter_noun2 + (" is ") + enter_color + ("."))
+print(enter_name + (" and her ") + enter_noun + (" like to go to the ") + enter_place + ("."))
+print(("While they are at the ") + enter_place + (" ") + enter_name + (" and her ") + enter_noun + (" see a ") + enter_adjective2 + enter_gender + ("."))
