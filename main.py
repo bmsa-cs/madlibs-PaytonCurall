98b7eae899164ea84aa49be0c702b399bd6daf32
Payton Curall <pcurall24@biomedscienceacademy.org>
2020-11-10 17:10:10 +0000

    Stuff

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ enter_animal2 = input("please enter an animal: ")
 enter_verb = input("please enter a verb: ")
 
 print(f"{enter_name} has a {enter_adjective} {enter_animal}. {enter_name}'s favorite color is {enter_color}. So everything in her house is {enter_color}, even her {enter_noun} is {enter_color}.\n {enter_name} and her {enter_animal} like to go to the {enter_place} together. While they are at the {enter_place} {enter_name} and her {enter_animal} see a {enter_adjective2} {enter_gender} walking a {enter_adjective3} {enter_animal2}.")
-print("As " + enter_name + " walks over to them the " + enter_adjective3 + " " + enter_animal2 +" " + enter_verb + "s the " + enter_adjective + " " + enter_animal")
+print("As" +enter_name+ "walks over to them, the " +enter_adjective3+ " " +enter_animal2+ " " +enter_verb+ "s the " +enter_adjective+ " " +enter_animal+ ")
